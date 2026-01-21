d384226bbcb81c980e873412fa845984c51b75b6
Hesron Janaran H. Carvalho Pereira <janaran52@gmail.com>
2026-01-21 17:34:48 -0300

    Primeiro commit

--- static/js/script.js
+++ static/js/script.js
@@ -471,3 +471,18 @@
     init();
 
 })();
+allCards.sort((a, b) => {
+    const dateA = a.querySelector('time')?.getAttribute('datetime');
+    const dateB = b.querySelector('time')?.getAttribute('datetime');
+    
+    if (!dateA && !dateB) return 0;
+    if (!dateA) return 1;
+    if (!dateB) return -1;
+    
+    return new Date(dateB) - new Date(dateA);
+});
+const cardsInGrid = blogGrid.querySelectorAll('.blog-card');
+cardsInGrid.forEach(card => card.remove());
+for (let i = start; i < end && i < allCards.length; i++) {
+    blogGrid.appendChild(allCards[i]);
+}
